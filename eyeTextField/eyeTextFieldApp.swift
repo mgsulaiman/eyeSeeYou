1b8c5f0f45b8efa3172acf4d560ce210d2e74db5
mgsulaiman <info@mgsulaiman.com>
2023-04-04 22:28:12 +0300

    Initial Commit

---
+++ eyeTextField/eyeTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  eyeTextFieldApp.swift
+//  eyeTextField
+//
+//  Created by developer mg on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct eyeTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
